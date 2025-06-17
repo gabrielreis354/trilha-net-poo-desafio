4098421390b5b840c2eb5f430172ccd9545c6bf0
Gabriel Reis <gabrielreiscunha6@gmail.com>
2025-06-17 16:58:45 -0300

    docs: desafio feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new("11999999999", "Nokia 6.1", "1234567890", 64);
+Iphone iphone = new("11988888888", "iPhone 12", "0987654321", 128);
+
+Console.WriteLine("Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nIphone:");
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
